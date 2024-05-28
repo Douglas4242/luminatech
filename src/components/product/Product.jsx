507fa1a178038256fd7ccf49a6533b9bd754b81d
Douglas Moraes <douglasgm89@live.com>
2024-05-28 13:48:14 -0300

    Products update
    Products section updated

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './product.css'
 
-const Product = ({ title, text }) => {
+const Product = ({ title, text, image }) => {
     return (
-        <div className='lt__product-container-product'>
-            <div className='lt__product-container-product-title'>
-                <div />
-                <h1>{title}</h1>
+        <div className='lt__product'>
+            <div className='lt__product-container-product'>
+                <div className='lt__product-container-product-title'>
+                    <div />
+                    <h1>{title}</h1>
+                </div>
+                <div className='lt__product-container-product-text'>
+                    <p>{text}</p>
+                </div>
             </div>
-            <div className='lt__product-container-product-text'>
-                <p>{text}</p>
+            <div className='lt__product-image'>
+                <img src={image} />
             </div>
         </div>
     )
--- src/containers/products/Products.jsx
+++ src/containers/products/Products.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './products.css'
 import { Product } from '../../components'
+import solarPannel from '../../assets/solarPannel.jpg'
+import energyStorage from '../../assets/energyStorage.jpeg'
+import LED from '../../assets/LED.jpg'
+import waterSave from '../../assets/waterSave.jpg'
 
 const Products = () => {
     return (
@@ -11,13 +15,18 @@ const Products = () => {
             </div>
             <div className='lt__products-product'>
                 <Product title="Painéis Solares"
-                    text="Painéis solares eficientes e duráveis, fabricados com tecnologia avançada, para uso residencial e empresarial." />
+                    text="Painéis solares eficientes e duráveis, fabricados com tecnologia avançada, para uso residencial e empresarial."
+                    image={solarPannel} />
                 <Product title="Sistemas de Armazenamento de Energia"
-                    text="Sistemas de baterias de alta capacidade para armazenar energia solar, garantindo energia contínua e sustentável." />
+                    text="Sistemas de baterias de alta capacidade para armazenar energia solar, garantindo energia contínua e sustentável."
+                    image={energyStorage} />
                 <Product title="Iluminação LED Eficiente"
-                    text="Luminárias LED eficientes para substituir lâmpadas convencionais, oferecendo economia de energia e longa vida útil." />
+                    text="Luminárias LED eficientes para substituir lâmpadas convencionais, oferecendo economia de energia e longa vida útil."
+                    image={LED} />
                 <Product title="Dispositivos de Economia de Água"
-                    text="Dispositivos para reduzir o consumo de água, como chuveiros de baixo fluxo e torneiras com sensores, promovendo sustentabilidade." />
+                    text="Dispositivos para reduzir o consumo de água, como chuveiros de baixo fluxo e torneiras com sensores, promovendo sustentabilidade."
+                    image={waterSave} />
+
             </div>
         </div>
     )
