2924159f9ff04290e636fd22f190cd27f7bebaab
Douglas Moraes <douglasgm89@live.com>
2024-05-27 21:14:26 -0300

    Navbar update
    Conclusion of the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/Logo.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+
+    const Menu = () => (
+        <>
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#products'>Produtos</a></p>
+            <p><a href='#tech'>Tecnologia</a></p>
+            <p><a href='#solutions'>Soluções</a></p>
+            <p><a href='#about'>Sobre</a></p>
+        </>
+    )
+
     return (
         <div className='lt__navbar'>
             <div className='lt__navbar-links'>
@@ -11,17 +24,29 @@ const Navbar = () => {
                     <img src={logo} alt='logo' />
                 </div>
                 <div className='lt__navbar-links_container'>
-                    <p><a href='#home'>Home</a></p>
-                    <p><a href='#products'>Produtos</a></p>
-                    <p><a href='#tech'>Tecnologia</a></p>
-                    <p><a href='#solutions'>Soluções</a></p>
-                    <p><a href='#about'>Sobre</a></p>
+                    <Menu />
                 </div>
             </div>
             <div className='lt__navbar-sign'>
                 <p>Entrar</p>
                 <button type='button'>Cadastrar</button>
-
+            </div>
+            <div className='lt__navbar-menu'>
+                {toggleMenu
+                    ? (< RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />)
+                    : (<RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />)
+                }
+                {toggleMenu && (
+                    <div className='lt__navbar-menu_container scale-up-center'>
+                        <div className='lt__navbar-menu_container-links'>
+                            <Menu />
+                            <div className='lt__navbar-menu_container-links-sign'>
+                                <p>Entrar</p>
+                                <button type='button'>Cadastrar</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
